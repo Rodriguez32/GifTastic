59edf650f8252b5174a40c2165752f40f1e9f619
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-07-13 17:37:08 -0500

    added to html and js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,30 +2,69 @@
 
 $(document).ready(function() {
 // array for buttons
-var topics = ["Parrot", "Owl", "Eagle", "Hummingbird"];
+var topics = ["Parrot" , "Owl" , "Eagle", "Hummingbird"];
 
-// function to display buttons
-function showgifButtons(){
-        $("#showButton").empty();
-        for(var i = 0; i < topics.length; i++) {
-                var gifButton = $("<button>");
-                console.log(gifButton);
-                gifButton.addClass("birds");
-                console.log(addClass);
-                gifButton.attr("data-name", topics[i]);
-                gifButton.text(topics[i]);
-                $("#showButton").append(gifButton);
-        }
+for (var i = 0; i < topics.length; i++) {
+        var button = $(" <button> " + topics[i] + " <button> ")
+       $("#showButton").append(button);
 }
+
+function addNewButton() {
+        $("addGif").on("click", function() {
+                  var bird = $("#topicInput").val().trim();
+                  if (bird == ""){
+                          return false;
+                  }
+                  topic.push(bird);
+
+                  showButton();
+                  return false;
+        });
+        
+}
+
+function showGifs(){
+        var birds = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        birds + "&api_key=IzkxH5nTOLLsfaxktzTqrDHHsNaWopf9&limit=10";
+        console.log(queryURL);
+
+        $.ajax({
+                url:queryURL,
+                method: "GET"
+        })
+
+        .then(function(response) {
+        console.log(response);        
+                
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                        var birdDiv = $("<div>");
+                        var rating = results[i].rating;
+                        var p = $("<p>").text("Rating: " + rating);
+
+                        var birdImage = $("<img>");
+
+                        birdImage.attr("src", results[i].images.fixed_height.url);
+                        console.log(birdImage);
+                        
+                        birdDiv.append(p);
+                        birdDiv.append(birdImage);
+
+                        $("#seeGifs").append(birdImage);     
+                }
+
+                
+        })
+
+}
+
 
 
 });
 
 
-
-//var bird = $(this).attr("data-bird");
-        //var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        //bird + "&api_key=IzkxH5nTOLLsfaxktzTqrDHHsNaWopf9&limit=10";
 
 
 
