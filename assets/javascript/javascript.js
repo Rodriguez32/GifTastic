4ac5c0ecdd0a7d6db8a47f1e3576d506d9518487
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-07-12 21:12:56 -0500

    changes to html and js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,45 +1,43 @@
 /* GifTastic */
-// button for event listener
-$("button").on("click", function() {
-        var bird = $(this).attr("data-bird");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        bird + "&api_key=IzkxH5nTOLLsfaxktzTqrDHHsNaWopf9&limit=10";
 
+$(document).ready(function() {
+// array for buttons
+var topics = ["Parrot", "Owl", "Eagle", "Hummingbird"];
 
-// loads AJAx call
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-        console.log(response);
-// the image is inside the data key. this variable set to results will bring image
-        var results = response.data;
-// for loop to put ratings on each gif
-        for (var i = 0; i < results.length; i++) {
-        var birdDiv = $("<div>");
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
-
-        var birdImage = $("<img>");
-
-        birdImage.attr("src", results[i].images.fixed_height.url);
-        console.log(birdImage);
-        birdImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
-        birdImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
-        birdImage.attr("data-state", "still"); // set the image state
-
-
-        birdDiv.append(p);
-        birdDiv.append(birdImage);
-
-        $("#gifs-go-here").append(birdDiv);   
-                
+// function to display buttons
+function showgifButtons(){
+        $("#showButton").empty();
+        for(var i = 0; i < topics.length; i++) {
+                var gifButton = $("<button>");
+                console.log(gifButton);
+                gifButton.addClass("birds");
+                console.log(addClass);
+                gifButton.attr("data-name", topics[i]);
+                gifButton.text(topics[i]);
+                $("#showButton").append(gifButton);
+        }
 }
 
-        
-        });
-        
+
 });
+
+
+
+//var bird = $(this).attr("data-bird");
+        //var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        //bird + "&api_key=IzkxH5nTOLLsfaxktzTqrDHHsNaWopf9&limit=10";
+
+
+
+
+
+
+
+
+
+        
+
+
 
 
 
